da14936aafb949bfb57239b4ee5db54e87a2e299
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-06 23:26:00 +0530

    Update module.js

--- module.js
+++ module.js
@@ -1,3 +1,7 @@
 const name = require('./data')
+//const{names,ages} = require('./data)
+//console.log(names,ages)
+console.log(name.ages) 
 
-console.log(name)
+const os = require('os')
+console.log(os.homedir())
