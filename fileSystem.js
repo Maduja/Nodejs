d28a4b3654099930add1979a819414d7965a8977
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-07 10:06:04 +0530

    file , folder deleted

--- fileSystem.js
+++ fileSystem.js
@@ -1,27 +1,46 @@
 const file = require("fs");
 
-if (!file.existsSync("./docs")) {
-  file.mkdir("./docs", (err) => {
-    if (err) {
-      console.log(err.message);
-    }
-    console.log("folder created");
-  });
-}
+// if (!file.existsSync("./docs")) {
+//   file.mkdir("./docs", (err) => {
+//     if (err) {
+//       console.log(err.message);
+//     }
+//     console.log("folder created");
+//   });
+// }
 
 
-file.writeFile('./docs/file.txt','hi hi hi',(err)=>{
+// file.writeFile('./docs/file.txt','hi hi hi',(err)=>{
+//     if(err){
+//         console.log(err.message)
+//     }else{
+//         console.log("file written")
+//     }
+// })
+
+// file.readFile('./docs/file.txt',(err,data)=>{
+//     if(err){
+//         console.log(err.message)
+//     }else{
+//         console.log(data.toString())
+//     }
+// })
+
+
+// if(file.existsSync('./docs/file.txt')){
+//     file.unlink('./docs/file.txt',(err)=>{
+//         if(err){
+//             console.log(err.message)
+//         }else{
+//             console.log('file deleted')
+//         }
+//     })
+// }
+
+file.rmdir('./docs',(err)=>{
     if(err){
         console.log(err.message)
     }else{
-        console.log("file written")
-    }
-})
-
-file.readFile('./docs/file.txt',(err,data)=>{
-    if(err){
-        console.log(err.message)
-    }else{
-        console.log(data.toString())
+        console.log("folder deleted")
     }
 })
