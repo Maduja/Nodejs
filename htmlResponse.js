a26aef3603a715bc2b37f07f2259a9acc0f1d759
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-08 23:14:31 +0530

    html-response

---
+++ htmlResponse.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req,res)=>{
+
+    res.setHeader('content-Type','text/html')
+    fs.readFile('./docs/index.html',(err,data)=>{
+        if(err){
+            console.log(err.message)
+            res.end()
+        }else{
+            res.write(data)
+            res.end()
+            //res.end(data)
+        }
+    })
+})
+server.listen(3000, 'localhost', ()=>{
+    console.log("Server is listening")
+})
--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log("Request Mode")
-    console.log(req.url)
-    console.log(req.method)
+    // console.log("Request Mode")
+    // console.log(req.url)
+    // console.log(req.method)
 
     //res.setHeader('content-Type','text/plain')
     res.setHeader('content-Type','text/html')
-    res.write("<h1>Subscribe to codeio</h1>")
-    res.write('<h4>Please do </h4>')
+    // res.write("<h1>Subscribe to codeio</h1>")
+    // res.write('<h4>Please do </h4>')
     res.end()
 })
 
