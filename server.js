20529e7697269d008473b093d00955b7d4101d33
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-08 22:54:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const http = require('http')
 
 const server = http.createServer((req,res)=>{
     console.log("Request Mode")
+    console.log(req.url)
+    console.log(req.method)
+
+    //res.setHeader('content-Type','text/plain')
+    res.setHeader('content-Type','text/html')
+    res.write("<h1>Subscribe to codeio</h1>")
+    res.write('<h4>Please do </h4>')
+    res.end()
 })
 
 server.listen(3000, 'localhost', ()=>{
