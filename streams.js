cb663d9c2bfd4cc7261b1f7dc237c0fbe340db32
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-08 20:36:36 +0530

    streams and buffers

--- streams.js
+++ streams.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 
-const readStream = fs.createReadStream('./docs/file.txt')
-readStream.on('data',(chunck)=>{
-    console.log(chunck.toString())
-})
+const readStream = fs.createReadStream('./docs/file.txt',{encoding:'utf-8'})
+const writeStream = fs.createWriteStream('./docs/newfile.txt')
+
+
+// readStream.on('data',(chunck)=>{
+//     writeStream.write(chunck)
+// })
+
+readStream.pipe(writeStream)
