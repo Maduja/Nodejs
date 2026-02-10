de14f903e894454e2291d8c3feea7cb6c85ee64f
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-10 09:58:38 +0530

    morgan 3rd party middleware

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 const exp = require('express')
-
+const morgan = require('morgan')
 const app = exp()
 
 app.listen(3000)
+
+//middleware
+// app.use((req,res,next)=>{
+//     console.log("Request recieved")
+//     console.log(req.host)
+//     console.log(req.path)
+//     next()
+// })
+
+app.use(morgan('dev'))
 
 app.get('/',(req,res)=>{
     //res.status(200).send('<h1>Codeio</h1>')
