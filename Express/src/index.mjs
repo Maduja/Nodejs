3ffd8812868f7586f1781f172a318da73c185733
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-10 20:57:12 +0530

    patch request

--- Express/src/index.mjs
+++ Express/src/index.mjs
@@ -64,6 +64,20 @@ app.put("/api/users/:id",(req,res)=>{
     
 })
 
+app.patch("/api/users/:id",(req,res)=>{
+    const id = parseInt(req.params.id)
+    if(isNaN(id)){
+        res.status(404).send({msg:"invalid"})
+    }
+    const userIndex = users.findIndex((user)=>user.id === id)
+    if(userIndex === -1){
+        return res.status(404).send({msg:"user not found"})
+    }
+    const {body} = req
+    users[userIndex] = {...users[userIndex], ...body}
+    return res.sendStatus(200)
+})
+
 app.listen(PORT,()=>{
     console.log(`App is running on port ${PORT}`)
 })
