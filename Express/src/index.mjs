41162bdacdc11d2cd7018317d7bc2c2502bd7b6c
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-11 20:47:15 +0530

    session

--- Express/src/index.mjs
+++ Express/src/index.mjs
@@ -1,29 +1,37 @@
-import express from "express"
-import { createUserValidationSchema } from "./utils/validationSchemas.mjs"
-import {validationResult, matchedData, checkSchema} from "express-validator"
-import userRouter from "./routes/users.mjs"
-import { users } from "./utils/constants.mjs" 
+import express from "express";
+import { createUserValidationSchema } from "./utils/validationSchemas.mjs";
+import { validationResult, matchedData, checkSchema } from "express-validator";
+import userRouter from "./routes/users.mjs";
+import { users } from "./utils/constants.mjs";
 //import { getUserIndexById } from "../src/utils/middlewares.mjs";
+import cookieParser from "cookie-parser";
+import session from 'express-session'
 
-const app = express()
-app.use(express.json())
-app.use(userRouter)
+const app = express();
+app.use(express.json());
+app.use(cookieParser("code io"));
 
-const PORT = 3000
+app.use(session({
+    secret:"romba secret",
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:60000 * 60
+    }
+}))
+app.use(userRouter);
 
+const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.cookie("user", "Admin", {maxAge: 60000 * 60, signed: true });
+  console.log(req.session)
+  console.log(req.session.id)
+  res.send({ msg: "Root" });
+});
 
-
-
-app.get('/',(req,res)=>{
-    res.send({msg:"Root"})
-})
-
-
-
-app.listen(PORT,()=>{
-    console.log(`App is running on port ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
 
 //localhost:3000/users?filter=user_name&value=go
