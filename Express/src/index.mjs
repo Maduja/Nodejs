ecd06847b4dcdd1fe447b46ffdbc47d90d6aa0ad
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-02-11 22:26:07 +0530

    passport module

--- Express/src/index.mjs
+++ Express/src/index.mjs
@@ -7,6 +7,9 @@ import { users } from "./utils/constants.mjs";
 import cookieParser from "cookie-parser";
 import session from 'express-session'
 
+import {Strategy as LocalStrategy} from "passport-local"
+import passport from "passport";
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser("code io"));
@@ -19,6 +22,32 @@ app.use(session({
         maxAge:60000 * 60
     }
 }))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(
+    {usernameField:"user_name", passwordField:"password"},
+    (user_name, password, done)=>{
+    const user = users.find((user)=>user.user_name===user_name)
+    if(!user){
+        return done(null, false, {message:"invalid username"})
+    }
+    if(user.password !== password){
+        return done(null, false, {message:"incorrect password"})
+    }
+    return done(null,user)
+}))
+
+passport.serializeUser((user,done)=>{
+    done(null,user.id)
+})
+
+passport.deserializeUser((id, done)=>{
+    const user = users.find((u)=>u.id===id)
+    done(null, user||false)
+})
+
 app.use(userRouter);
 
 const PORT = 3000;
@@ -29,6 +58,23 @@ app.get("/", (req, res) => {
   console.log(req.session.id)
   res.send({ msg: "Root" });
 });
+
+
+app.post("/login", (req,res,next)=>{
+    passport.authenticate("local",(err, user, info)=>{
+        if(err){
+            return next(err)
+        }if(!user){
+            return res.status(401).json({message: info?.message || "Login failed"})
+        }
+        req.logIn(user, (err)=>{
+            if(err){
+                return next(err)
+            }
+            return res.json({message:"login succesfull", user})
+        })
+    })(req,res, next)
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
--- Express/src/utils/constants.mjs
+++ Express/src/utils/constants.mjs
@@ -1,7 +1,7 @@
 export const users =[
-    {id:1, user_name:"goms"},
-    {id:2, user_name:"madu"},
-    {id:3, user_name:"siva"},
-    {id:4, user_name:"kamal"},
-    {id:5, user_name:"hasini"},
+    {id:1, user_name:"goms", password:"1234"},
+    {id:2, user_name:"madu", password:"234"},
+    {id:3, user_name:"siva", password:"34"},
+    {id:4, user_name:"kamal", password:"67"},
+    {id:5, user_name:"hasini", password:"8974"},
 ]
